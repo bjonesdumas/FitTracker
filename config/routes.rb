c50ccecca2e36f24eb4698e24615a1fb747ff58c
bjonesdumas <bjonesdumas@gmail.com>
2016-01-15 05:44:25 -0500

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,24 @@
+Rails.application.routes.draw do
+  devise_for :users, :controllers => { registrations: 'registrations'}
+  get 'dashboard/index'
+  
+  resources :users do
+    resources :exercises
+  end
+  
+  resource :dashboard, only: [:index] do
+    collection do
+      post :search, to: 'dashboard#search'
+    end
+  end
+  
+  resources :friendships, only: [:show, :create, :destroy]
+
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
+
+  # You can have the root of your site routed with "root"
+  root to: 'dashboard#index'
+
+  
+end
